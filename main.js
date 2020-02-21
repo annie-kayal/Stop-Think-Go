d3bd2a60a582d982585c9261dc68ed0cbf61bdf7
annie-kayal <anamikakayal@hotmail.com>
2020-02-21 14:42:36 +0000

    one car moves

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ function gameSetUp() {
   const grid = document.querySelector('.grid')
   const cells = []
   let hog = 90
+  let car = 2
+  let points = 0
+
 
   // create grid
   for (let i = 0; i < gridCellCount; i++) {
@@ -48,5 +51,31 @@ function gameSetUp() {
       cells[hog].classList.add('hog')
     }
   })
+
+  // add car every 3 seconds
+  const obstucleAdd = setInterval(() => {
+    if (car + 10) {
+      cells[2].classList.add('car')
+    }
+  }, 3000)
+
+  cells[car].classList.add('car')
+  const obstucleMove = setInterval(() => {
+    if (cells[92].classList.contains('car')) {
+      cells[car].classList.remove('car')
+      clearInterval(obstucleMove)
+    } else {
+      cells[car].classList.remove('car')
+      car += 10
+      cells[car].classList.add('car')
+    }
+  }, 1000)
 }
+
+
+// function TotalScore() {
+//   if (cells[hog] + 1) {
+//     + 10
+//   }
+// }
 window.addEventListener('DOMContentLoaded', gameSetUp)
