e265257a381c2cf945bf2988b1f58123133f9734
annie-kayal <anamikakayal@hotmail.com>
2020-02-22 15:27:48 +0000

    multiple car moving

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ function gameSetUp() {
   let hog = 90
   let car = 2
   let points = 0
+  // array for obstucle position when page loads
+  const startingPositionObstucles = [2, 22, 42, 62]
+  // array to identify the bottom of the grid
+  const borderBottom = [92,93,94,95,96,97,98]
+  // array to be called on for random generation of class
+  const obstacleClassArray = ['car', 'giraffe', 'elephant'] 
 
 
   // create grid
@@ -14,6 +20,9 @@ function gameSetUp() {
     cell.classList.add('cell')
     if (i === hog) {
       cell.classList.add('hog')
+    } 
+    if (startingPositionObstucles.includes(i)) {
+      cell.classList.add('car')
     }
     cells.push(cell)
     grid.appendChild(cell)
@@ -52,24 +61,27 @@ function gameSetUp() {
     }
   })
 
-  // add car every 3 seconds
-  const obstucleAdd = setInterval(() => {
-    if (car + 10) {
-      cells[2].classList.add('car')
-    }
-  }, 3000)
+  // if you hit the bottom, if sttements to see what the class is per element
+  // in obstucleClassArray 
 
-  cells[car].classList.add('car')
-  const obstucleMove = setInterval(() => {
-    if (cells[92].classList.contains('car')) {
-      cells[car].classList.remove('car')
-      clearInterval(obstucleMove)
-    } else {
-      cells[car].classList.remove('car')
-      car += 10
-      cells[car].classList.add('car')
+  const randomObstacleSelectionArray = obstacleClassArray[Math.floor(Math.random() * obstacleClassArray.length)]
+  console.log(randomObstacleSelectionArray)
+
+  // make car move every second
+  const carInterval = setInterval(() => {
+    for (let i = 0; i < startingPositionObstucles.length; i++) {
+      if (borderBottom.includes(startingPositionObstucles[i])) {
+        startingPositionObstucles.push(randomObstacleSelectionArray)
+        cells[startingPositionObstucles[i]].classList.remove('car')
+        startingPositionObstucles[i] -= (width ** 2 - 10)
+        cells[startingPositionObstucles[i]].classList.add('car')        
+      } else {
+        cells[startingPositionObstucles[i]].classList.remove('car')
+        startingPositionObstucles[i] += width
+        cells[startingPositionObstucles[i]].classList.add('car')
+      }
     }
-  }, 1000)
+  }, 900)
 }
 
 
